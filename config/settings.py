2d97d22de652746eb95643abbbc0ff19f1596c3b
khawle-sk <24052@supnum.mr>
2026-06-27 19:14:05 +0000

    Ajout de PostgreSQL Supabase

--- config/settings.py
+++ config/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Si le site est sur Render, il prend la base Supabase. Sinon, il garde SQLite sur ton PC pour tester.
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
